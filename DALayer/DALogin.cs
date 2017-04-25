c40c1911b8726cbc79b8da1adc060808b0c1379e
hezhangxi <hezhangxi@qq.com>
2017-04-25 09:41:37 +0800

    初始版本

---
+++ DALayer/DALogin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace DALayer
+{
+   public class DALogin
+    {
+     
+       Sqlconn mySqlconn = new Sqlconn();//实例化类
+       string SQL = string.Empty;//保持SQL语句
+
+
+       /// <summary>
+       ///   //判断用户是否存在,第一行第一例是否有值
+       /// </summary>
+       /// <param name="paramUserName"></param>
+       /// <returns></returns>
+       public string GetUserInfo(string paramUserName)
+       {
+           string RetuenValue=string .Empty ;
+           SQL = "select username from adminuser where username = '" + paramUserName + "'";
+           RetuenValue = mySqlconn.ExecuteSql(SQL);
+           return RetuenValue;
+       }
+
+       /// <summary>
+       /// 通过已有的用户 获取用户表信息
+       /// </summary>
+       /// <param name="paramUserName"></param>
+       /// <returns></returns>
+       public DataTable GetUserData(string paramUserName) 
+       {
+           DataTable myDt = new DataTable();
+           SQL = "select * from adminuser where username = '" + paramUserName + "'";
+           DataSet myDs = new DataSet();
+           myDs = mySqlconn.Query(SQL);
+           myDt = myDs.Tables[0];
+           return myDt;
+       }
+    }
+}
---
+++ InventoryManagersystem/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace InventoryManagersystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(833, 68);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.menuStrip1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 68);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(833, 25);
+            this.panel2.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(833, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(136, 21);
+            this.toolStripMenuItem1.Text = "toolStripMenuItem1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 553);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+    }
+}
